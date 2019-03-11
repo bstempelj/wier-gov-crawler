0c78e4fd6d9b71246a44fb046068a59f92f2414b
Blaž Štempelj <blaz.stempelj@gmail.com>
2019-03-11 20:50:55 +0100

    geckodriver works

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello World")
+from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support import expected_conditions as expected
+from selenium.webdriver.support.wait import WebDriverWait
+
+if __name__ == "__main__":
+  options = Options()
+  options.add_argument('-headless')
+  driver = Firefox(executable_path='geckodriver', options=options)
+  wait = WebDriverWait(driver, timeout=10)
+  driver.get('http://evem.gov.si')
+  # driver.execute_script('Location()')
+  print(driver.page_source)
+  driver.quit()
