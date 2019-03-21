2f9fb9933e9fec6079049240a61142c7695fbfc8
Blaz Stempelj <blaz.stempelj@gmail.com>
2019-03-21 09:47:27 +0100

    added sitemap parser to main project

---
+++ src/sitemap_parser.py
@@ -0,0 +1,30 @@
+import requests
+import xml.etree.ElementTree as ET
+
+
+class SitemapParser:
+	def __init__(self):
+		self._sitemaps = []
+		self.urls = []
+
+	def _parse_sitemap(self, sitemap):
+		root = ET.fromstring(sitemap)
+		for url in root:
+			for prop in url:
+				if prop.tag.endswith("loc"):
+					self.urls.append(prop.text)
+
+	def find_sitemaps(self, robots_url):
+		self._sitemaps[:] = []	# clear previous entries
+		r = requests.get(robots_url)
+		if r.status_code == 200:
+			for line in r.text.splitlines():
+				line = line.strip().split(':', 1)
+				if line[0].lower() == "sitemap":
+					self._sitemaps.append(line[1].strip())
+
+	def parse_sitemaps(self):
+		for sm in self._sitemaps:
+			r = requests.get(sm)
+			if r.status_code == 200:
+				self._parse_sitemap(r.text)
