6dada1d3c9597c852e9f04eb639705a6c929613b
Blaz Stempelj <blaz.stempelj@gmail.com>
2019-03-29 15:36:39 +0100

    moved image downloading to demos folder (not yet used)

--- src/main.py
+++ src/main.py
@@ -28,8 +28,8 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 class Browser(Enum):
-    FIREFOX = 1
-    CHROME = 2
+	FIREFOX = 1
+	CHROME = 2
 
 
 seed = ["http://evem.gov.si", "http://e-uprava.gov.si", "http://podatki.gov.si", "http://e-prostor.gov.si"]
@@ -37,126 +37,102 @@ img_folder = "images"
 browser = Browser.FIREFOX
 
 
-def norm_url(url):
-    q = url.find("?")
-    if q != -1:
-        return url[:q]
-    return url
-
-
 def get_base_url(url):
-    split_url = urlsplit(url)
-    return "://".join([split_url.scheme, split_url.netloc])
+	split_url = urlsplit(url)
+	return "://".join([split_url.scheme, split_url.netloc])
 
 
 def has_robots_file(url):
-    r = requests.get(get_base_url(url) + "/robots.txt")
-    return r.status_code == 200, r.text
+	r = requests.get(get_base_url(url) + "/robots.txt")
+	return r.status_code == 200, r.text
 
 
 def get_urls(driver, frontier):
-    # for n in driver.find_elements_by_xpath("//*[@onclick]"):
-    #     print(n)
+	# for n in driver.find_elements_by_xpath("//*[@onclick]"):
+	#     print(n)
 
-    for n in driver.find_elements_by_xpath("//a[@href]"):
-        link = n.get_attribute("href")
-        if len(link) > 0 and link != "javascript:void(0)":
-            frontier.add_url(link)
+	for n in driver.find_elements_by_xpath("//a[@href]"):
+		link = n.get_attribute("href")
+		if len(link) > 0 and link != "javascript:void(0)":
+			frontier.add_url(link)
 
-
-def save_img(url):
-    url = norm_url(url)
-    filename, ext = splitext(url)
-    if ext in [".png", ".jpg", ".jpeg", ".gif"]:
-        filename = basename(filename)
-        print("Downloading: %s" % filename)
-        r = requests.get(url)
-        i = Image.open(BytesIO(r.content))
-        i.save("images/%s%s" % (filename, ext))
 
 def check_if_doc(url):
-    url = norm_url(url)
-    url, ext = splitext(url)
-    if ext in [".doc", ".docx", ".pdf", ".xlsx", ".xls", ".PPT"]:
-        pass
+	url = norm_url(url)
+	url, ext = splitext(url)
+	if ext in [".doc", ".docx", ".pdf", ".xlsx", ".xls", ".PPT"]:
+		pass
 
 
 if __name__ == "__main__":
 
-    if browser == Browser.FIREFOX:
-        options = FirefoxOptions()
-        options.headless = True
-        driver = Firefox(executable_path="geckodriver", options=options)
-    else:
-        options = ChromeOptions()
-        options.headless = True
-        driver = Chrome(executable_path="chromedriver", options=options)
+	if browser == Browser.FIREFOX:
+		options = FirefoxOptions()
+		options.headless = True
+		driver = Firefox(executable_path="geckodriver", options=options)
+	else:
+		options = ChromeOptions()
+		options.headless = True
+		driver = Chrome(executable_path="chromedriver", options=options)
 
-    frontier = Frontier()
-    robots = []
-    rp = RobotFileParser()
-    sp = SitemapParser()
-    # db = Database()
-    site_id = -1
+	frontier = Frontier()
+	robots = []
+	rp = RobotFileParser()
+	sp = SitemapParser()
+	# db = Database()
+	site_id = -1
 
-    frontier.add_urls(seed)
-    while frontier.has_urls() and not frontier.max_reached() and i < 10:
-        # url info -
-        url = frontier.get_next().replace("www.", "")
-        base_url = get_base_url(url)
-        robots_url = base_url + "/robots.txt"
+	frontier.add_urls(seed)
+	while frontier.has_urls() and not frontier.max_reached():
+		# url info -
+		url = frontier.get_next().replace("www.", "")
+		base_url = get_base_url(url)
+		robots_url = base_url + "/robots.txt"
 
-        # Check that we are still on track
-        if base_url not in seed:
-            continue
+		# Check that we are still on track
+		if base_url not in seed:
+			continue
 
-        # connect to website
-        http_head = requests.head(url)  # .page_source v bazo
-        driver.get(url)
+		# connect to website
+		http_head = requests.head(url)  # .page_source v bazo
+		driver.get(url)
 
-        # check for robots.txt
-        robot_file = has_robots_file(url)
-        if base_url not in robots and robot_file[0]:
-            robots.append(base_url)
+		# check for robots.txt
+		robot_file = has_robots_file(url)
+		if base_url not in robots and robot_file[0]:
+			robots.append(base_url)
 
-        # parse sitemap
-        sp.find_sitemaps(base_url)
-        sp.parse_sitemaps()
-        print(sp.urls)
+		# parse sitemap
+		sp.find_sitemaps(base_url)
+		sp.parse_sitemaps()
 
-        # respect robots.txt
-        if base_url in robots:
-            rp.set_url(robots_url)
-            rp.read()
+		# respect robots.txt
+		if base_url in robots:
+			rp.set_url(robots_url)
+			rp.read()
 
-            # parse sitemap
-            sp.find_sitemaps(robots_url)
-            sp.parse_sitemaps()
+			# parse sitemap
+			sp.find_sitemaps(base_url)
+			sp.parse_sitemaps()
+			frontier.add_urls(sp.urls)
 
-            # Write site to database
-            # db.add_site(base_url, robot_file[1], sitemaps)
-            frontier.add_urls(sp.urls)
+			# Write site to database
+			# db.add_site(base_url, robot_file[1], sitemaps)
 
-            if rp.can_fetch("*", url):
-                get_urls(driver, frontier)
-        else:
-            # no robots.txt => parse everything :)
+			if rp.can_fetch("*", url):
+				get_urls(driver, frontier)
+		else:
+			# no robots.txt => parse everything :)
+			get_urls(driver, frontier)
 
-            get_urls(driver, frontier)
+		date_res = None
+		if 'Date' in http_head.headers:
+			date_res = http_head.headers['Date']
 
-        date_res = None
-        if 'Date' in http_head.headers:
-            date_res = http_head.headers['Date']
+		# db.add_page(http_head.url, driver.page_source, http_head.status_code, date_res)
 
-        # db.add_page(http_head.url, driver.page_source, http_head.status_code, date_res)
+	driver.close()
 
-        # get all images from a site
-        # for n in driver.find_elements_by_tag_name("//img[@src]"):
-        #     img_url = n.get_attribute("src")
-        #     save_img(img_url)
-
-    driver.close()
-
-    # print history
-    # for url in frontier._history.values():
-    #     print(url)
+	# print history
+	# for url in frontier._history.values():
+	#     print(url)
