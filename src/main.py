18bb07c20f741846cbfc1b6b3c6313615e787ae9
Denis Grabljevec <denis@kemikalije.com>
2019-04-01 16:40:28 +0200

    Stash

--- src/main.py
+++ src/main.py
@@ -35,203 +35,205 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 
 class Browser(Enum):
-	FIREFOX = 1
-	CHROME = 2
+    FIREFOX = 1
+    CHROME = 2
 
 
-use_database = False
-seed = ["http://evem.gov.si", "http://e-uprava.gov.si", "http://podatki.gov.si", "http://e-prostor.gov.si"]
-browser = Browser.FIREFOX
+use_database = True
+seed = ["http://e-prostor.gov.si"]
+browser = Browser.CHROME
 counter = 0
 
 def get_base_url(url):
-	split_url = urlsplit(url)
-	return "://".join([split_url.scheme, split_url.netloc])
+    split_url = urlsplit(url)
+    return "://".join([split_url.scheme, split_url.netloc])
 
 
 def init_sites():
-	global seed
+    global seed
 
-	for idx, site in enumerate(seed):
-		base_url = get_base_url(site)
-		robots_url = base_url + "/robots.txt"
-		frontier.add_url(base_url, 1)
+    for idx, site in enumerate(seed):
+        base_url = get_base_url(site)
+        robots_url = base_url + "/robots.txt"
+        frontier.add_url(base_url, 1)
 
-		# check for robots.txt
-		robot_file = has_robots_file(base_url)
-		if base_url not in robots and robot_file[0]:
-			robots.append(base_url)
+        # check for robots.txt
+        robot_file = has_robots_file(base_url)
+        if base_url not in robots and robot_file[0]:
+            robots.append(base_url)
 
-		if base_url in robots:
-			# parse sitemap
-			sp.find_sitemaps(base_url)
-			sp.parse_sitemaps()
+        if base_url in robots:
+            # parse sitemap
+            sp.find_sitemaps(base_url)
+            sp.parse_sitemaps()
 
-			# Write site to database
-			db.add_site(base_url, robot_file[1], sp.urls_to_string())
-		else:
-			db.add_site(base_url, None, None)
+            # Write site to database
+            db.add_site(base_url, robot_file[1], sp.urls_to_string())
+            # frontier.add_urls(sp.urls)
+        else:
+            db.add_site(base_url, None, None)
 
 
 def has_robots_file(url):
-	r = requests.get(get_base_url(url) + "/robots.txt")
-	return r.status_code == 200, r.text
+    r = requests.get(get_base_url(url) + "/robots.txt")
+    return r.status_code == 200, r.text
 
 
 def contains(s, attrs):
-	res = [s.find(a) != -1 for a in attrs]
-	return True in res
+    res = [s.find(a) != -1 for a in attrs]
+    return True in res
 
 
 def get_urls(driver, frontier, page_id):
-	# Parsing onClick
-	# for n in driver.find_elements_by_xpath("//*[@onclick]"):
-	#     onclick = n.get_attribute("onclick")
-	#     link = ""
+    # Parsing onClick
+    # for n in driver.find_elements_by_xpath("//*[@onclick]"):
+    #     onclick = n.get_attribute("onclick")
+    #     link = ""
 
-	#     if contains(onclick, ["parent.open", "window.open"]):
-	#         link = onclick.split("(")[1]
-	#         link = link.split(",")
-	#         link = link[0] if len(link) != 1 else link[0][:-1]
-	#     elif contains(onclick, ["location.href", "parent.location"]):
-	#         link = onclick.split("=")[1].strip()
+    #     if contains(onclick, ["parent.open", "window.open"]):
+    #         link = onclick.split("(")[1]
+    #         link = link.split(",")
+    #         link = link[0] if len(link) != 1 else link[0][:-1]
+    #     elif contains(onclick, ["location.href", "parent.location"]):
+    #         link = onclick.split("=")[1].strip()
 
-	#     if link != "":
-	#         print("FOUND:", link)
+    #     if link != "":
+    #         print("FOUND:", link)
 
-	# Parsing links
-	for n in driver.find_elements_by_xpath("//a[@href]"):
-		try:
-			url = n.get_attribute("href")
-			if len(url) > 0 and url.find('javascript:void(0)') == -1:
-				frontier.add_url(url, page_id)
-		except Exception as e:
-			print(e)
+    # Parsing links
+    for n in driver.find_elements_by_xpath("//a[@href]"):
+        try:
+            url = n.get_attribute("href")
+            if len(url) > 0 and url.find('javascript:void(0)') == -1:
+                frontier.add_url(url, page_id)
+        except Exception as e:
+            print(e)
 
-	# Parsing images
-	for n in driver.find_elements_by_xpath("//img[@src]"):
-		try:
-			url = n.get_attribute("src")
-			filename, ext = splitext(url)
-			if ext in [".png", ".jpg", ".jpeg", ".gif"]:
-				frontier.add_url(url, page_id)
-		except Exception as e:
-			print(e)
+    # Parsing images
+    for n in driver.find_elements_by_xpath("//img[@src]"):
+        try:
+            url = n.get_attribute("src")
+            filename, ext = splitext(url)
+            if ext in [".png", ".jpg", ".jpeg", ".gif"]:
+                frontier.add_url(url, page_id)
+        except Exception as e:
+            print(e)
 
 
 def init_browser():
-	# Driver for selenium
-	if browser == Browser.FIREFOX:
-		options = FirefoxOptions()
-		options.headless = True
-		driver = Firefox(executable_path="geckodriver", options=options)
-	else:
-		options = ChromeOptions()
-		options.headless = True
-		driver = Chrome(executable_path="chromedriver", options=options)
+    # Driver for selenium
+    if browser == Browser.FIREFOX:
+        options = FirefoxOptions()
+        options.headless = True
+        driver = Firefox(executable_path="geckodriver", options=options)
+    else:
+        options = ChromeOptions()
+        options.headless = True
+        driver = Chrome(executable_path="chromedriver", options=options)
 
-	return driver
+    return driver
 
 
 def crawler(th_num, frontier, db, rp, sp, robots, start):
-	global counter
-	driver = init_browser()
+    global counter
+    driver = init_browser()
 
-	while frontier.has_urls():
-		# url info -
-		frontier_data = frontier.get_next()
-		url = frontier_data[0]
-		base_url = get_base_url(url)
-		robots_url = base_url + "/robots.txt"
+    while frontier.has_urls():
+        # url info -
+        frontier_data = frontier.get_next()
+        url = frontier_data[0]
+        base_url = get_base_url(url)
+        robots_url = base_url + "/robots.txt"
 
-		# Check that we are still on track
-		if base_url not in seed:
-			continue
+        # Check that we are still on track
+        if base_url not in seed:
+            continue
 
-		# connect to website
-		print('Thread: ' + th_num + ' - ' + url)
-		http_head = requests.head(url)  # .page_source v bazo
+        # connect to website
+        print('Thread: ' + th_num + ' - ' + url)
+        http_head = requests.head(url)  # .page_source v bazo
 
-		# Skip links which give 404 not found
-		# (links still exists but file doesnt anymore)
-		if http_head.status_code == 404:
-			continue
+        # Skip links which give 404 not found
+        # (links still exists but file doesnt anymore)
+        if http_head.status_code == 404:
+            continue
 
-		try:
-			driver.get(url)
-		except Exception as e:
-			print(e)
-			driver = init_browser()
-			continue
+        try:
+            driver.get(url)
+        except Exception as e:
+            print(e)
+            driver = init_browser()
+            continue
 
-		date_res = None
-		if 'Date' in http_head.headers:
-			date_res = http_head.headers['Date']
+        date_res = None
+        if 'Date' in http_head.headers:
+            date_res = http_head.headers['Date']
 
-		site_id = seed.index(base_url)+1
-		page_id = db.add_page(http_head.url, driver.page_source, http_head.status_code, date_res, site_id, frontier_data[1], frontier.get_url_hash(driver.page_source))
+        site_id = seed.index(base_url)+1
+        page_id = db.add_page(http_head.url, driver.page_source, http_head.status_code, date_res, site_id, frontier_data[1], frontier.get_url_hash(driver.page_source))
 
-		counter += 1
-		if (counter % 1000) == 0:
-			end = time.time()
-			print((end - start) / 60)
-			start = time.time()
+        counter += 1
+        if (counter % 1000) == 0:
+            end = time.time()
+            print((end - start) / 60)
+            start = time.time()
 
-		is_html = True
-		if 'Content-Type' in http_head.headers:
-			con_type = http_head.headers['Content-Type']
-			is_html = True if con_type.find('html', 0) > -1 else False
+        is_html = True
+        if 'Content-Type' in http_head.headers:
+            con_type = http_head.headers['Content-Type']
+            is_html = True if con_type.find('html', 0) > -1 else False
 
-		# respect robots.txt
-		if base_url in robots and is_html:
-			rp.set_url(robots_url)
-			rp.read()
+        # respect robots.txt
+        if base_url in robots and is_html:
+            rp.set_url(robots_url)
+            rp.read()
 
-			if rp.can_fetch("*", url):
-				get_urls(driver, frontier, page_id)
-		elif is_html:
-			# no robots.txt => parse everything :)
-			# Write site to database without
-			get_urls(driver, frontier, page_id)
+            if rp.can_fetch("*", url):
+                get_urls(driver, frontier, page_id)
+        elif is_html:
+            # no robots.txt => parse everything :)
+            # Write site to database without
+            get_urls(driver, frontier, page_id)
 
-		if not frontier.has_urls():
-			print(th_num + " sleep")
-			time.sleep(10)
+        if not frontier.has_urls():
+            print(th_num + " sleep")
+            time.sleep(10)
 
-	driver.close()
+    driver.close()
 
 
 if __name__ == "__main__":
-	frontier = Frontier(seed)
-	robots = []
-	rp = RobotFileParser()
-	sp = SitemapParser()
-	db = Database(use_database)
+    frontier = Frontier()
+    robots = []
+    rp = RobotFileParser()
+    sp = SitemapParser()
+    db = Database(use_database)
 
-	init_sites()
-	print(robots)
-	start = time.time()
+    init_sites()
+    print(robots)
+    start = time.time()
 
-	# Read thread num argument
-	thread_num = 1
-	if len(sys.argv) > 1:
-		thread_num = int(sys.argv[1])
-	else:
-		crawler('1', frontier, db, rp, sp, robots, start)
+    # Read thread num argument
+    thread_num = 1
+    print(sys.argv)
+    if len(sys.argv) > 1:
+        thread_num = int(sys.argv[1])
 
-	if thread_num > 1:
-		th_list = list()
-		for i in range(thread_num):
-			th_list.append(th.Thread(target=crawler, args=(str(i), frontier, db, rp, sp, robots, start)))
+    if thread_num > 1:
+        th_list = list()
+        for i in range(thread_num):
+            th_list.append(th.Thread(target=crawler, args=(str(i), frontier, db, rp, sp, robots, start)))
 
-		for i in range(thread_num):
-			th_list[i].start()
+        for i in range(thread_num):
+            th_list[i].start()
 
-		for i in range(thread_num):
-			th_list[i].join()
+        for i in range(thread_num):
+            th_list[i].join()
+    else:
+        crawler('1', frontier, db, rp, sp, robots, start)
 
-	# print history
-	for url in frontier._history.values():
-		print(url)
+    # print history
+    for url in frontier._history.values():
+        print(url)
 
 
